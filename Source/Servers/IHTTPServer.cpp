1c458ff0d7a125e86412117de7ebd1a1d3efab4b
Convery <convery10122@gmail.com>
2016-05-06 21:05:14 +0200

    Implemented a proper IHTTPServer.

---
+++ Source/Servers/IHTTPServer.cpp
@@ -0,0 +1,271 @@
+/*
+    Initial author: (https://github.com/)Convery for Ayria.se
+    License: LGPL 3.0
+    Started: 2016-5-6
+    Notes:
+        Implements parsing of the HTTP protocol.
+*/
+
+#include <Configuration\All.h>
+#include <Servers\IHTTPServer.h>
+
+// Callbacks for parsing.
+size_t Parse_Messagebegin(http_parser *Parser, HTTPRequest *Request)
+{
+    Request->Headers.clear();
+    return 0;
+}
+size_t Parse_URL(http_parser *Parser, HTTPRequest *Request, const char *Data, size_t Length)
+{
+    Request->URL = Data;
+    return 0;
+}
+size_t Parse_Headerfield(http_parser *Parser, HTTPRequest *Request, const char *Data, size_t Length)
+{
+    HTTPHeader Header;
+    Header.Field = Data;
+
+    Request->Headers.push_back(Header);
+    return 0;
+}
+size_t Parse_Headervalue(http_parser *Parser, HTTPRequest *Request, const char *Data, size_t Length)
+{
+    HTTPHeader *Header = &Request->Headers.back();
+    Header->Value = Data;
+    return 0;
+}
+size_t Parse_Headerscomplete(http_parser *Parser, HTTPRequest *Request)
+{
+    Request->Method = http_method_str((http_method)Parser->method);
+    return 0;
+}
+size_t Parse_Body(http_parser *Parser, HTTPRequest *Request, const char *Data, size_t Length)
+{
+    Request->Body = Data;
+    return 0;
+}
+size_t Parse_Messagecomplete(http_parser *Parser, HTTPRequest *Request)
+{
+    Request->Parsed = true;
+    return 0;
+}
+
+// Callbacks on incoming data.
+void IHTTPServer::onStreamupdated(std::vector<uint8_t> &Incomingstream)
+{
+    if (Parsedrequest.Parsed)
+        std::memset(&Parsedrequest, 0, sizeof(Parsedrequest));
+
+    // Parse the incomming data.
+    http_parser_execute(&Parser, &Parsersettings, (const char *)Incomingstream.data(), Incomingstream.size());
+
+    if (Parsedrequest.Parsed)
+    {
+        switch (FNV1a_Runtime_32(Parsedrequest.Method.c_str(), Parsedrequest.Method.size()))
+        {
+            case FNV1a_Compiletime_32("GET"): onGET(Parsedrequest.URL, Parsedrequest.Body); break;
+            case FNV1a_Compiletime_32("PUT"): onPUT(Parsedrequest.URL, Parsedrequest.Body); break;
+            case FNV1a_Compiletime_32("POST"): onPOST(Parsedrequest.URL, Parsedrequest.Body); break;        
+        }
+    }
+}
+void IHTTPSServer::onStreamdecrypted(std::string &Incomingstream)
+{
+    if (Parsedrequest.Parsed)
+        std::memset(&Parsedrequest, 0, sizeof(Parsedrequest));
+
+    // Parse the incomming data.
+    http_parser_execute(&Parser, &Parsersettings, (const char *)Incomingstream.data(), Incomingstream.size());
+
+    if (Parsedrequest.Parsed)
+    {
+        switch (FNV1a_Runtime_32(Parsedrequest.Method.c_str(), Parsedrequest.Method.size()))
+        {
+            case FNV1a_Compiletime_32("GET"): onGET(Parsedrequest.URL, Parsedrequest.Body); break;
+            case FNV1a_Compiletime_32("PUT"): onPUT(Parsedrequest.URL, Parsedrequest.Body); break;
+            case FNV1a_Compiletime_32("POST"): onPOST(Parsedrequest.URL, Parsedrequest.Body); break;        
+        }
+    }
+}
+
+// Construct the server from a hostname.
+IHTTPServer::IHTTPServer() : ITCPServer() 
+{
+    http_parser_init(&Parser, HTTP_BOTH);
+    http_parser_settings_init(&Parsersettings);
+    Parsedrequest.Parsed = false;
+    Parser.data = &Parsedrequest;
+
+    Parsersettings.on_message_begin = [](http_parser *parser) -> int
+    {
+        return Parse_Messagebegin(parser, (HTTPRequest *)parser->data);
+    };
+    Parsersettings.on_url = [](http_parser* parser, const char* at, size_t len) -> int
+    {
+        return Parse_URL(parser, (HTTPRequest *)parser->data, at, len);
+    };
+    Parsersettings.on_header_field = [](http_parser* parser, const char* at, size_t len) -> int
+    {
+        return Parse_Headerfield(parser, (HTTPRequest *)parser->data, at, len);
+    };
+    Parsersettings.on_header_value = [](http_parser* parser, const char* at, size_t len) -> int
+    {
+        return Parse_Headervalue(parser, (HTTPRequest *)parser->data, at, len);
+    };
+    Parsersettings.on_headers_complete = [](http_parser* parser) -> int
+    {
+        return Parse_Headerscomplete(parser, (HTTPRequest *)parser->data);
+    };
+    Parsersettings.on_body = [](http_parser* parser, const char* at, size_t len) -> int
+    {
+        return Parse_Body(parser, (HTTPRequest *)parser->data, at, len);
+    };
+    Parsersettings.on_message_complete = [](http_parser* parser) -> int
+    {
+        return Parse_Messagecomplete(parser, (HTTPRequest *)parser->data);
+    };
+};
+IHTTPServer::IHTTPServer(const char *Hostname) : ITCPServer(Hostname) 
+{
+    http_parser_init(&Parser, HTTP_BOTH);
+    http_parser_settings_init(&Parsersettings);
+    Parsedrequest.Parsed = false;
+    Parser.data = &Parsedrequest;
+
+    Parsersettings.on_message_begin = [](http_parser *parser) -> int
+    {
+        return Parse_Messagebegin(parser, (HTTPRequest *)parser->data);
+    };
+    Parsersettings.on_url = [](http_parser* parser, const char* at, size_t len) -> int
+    {
+        return Parse_URL(parser, (HTTPRequest *)parser->data, at, len);
+    };
+    Parsersettings.on_header_field = [](http_parser* parser, const char* at, size_t len) -> int
+    {
+        return Parse_Headerfield(parser, (HTTPRequest *)parser->data, at, len);
+    };
+    Parsersettings.on_header_value = [](http_parser* parser, const char* at, size_t len) -> int
+    {
+        return Parse_Headervalue(parser, (HTTPRequest *)parser->data, at, len);
+    };
+    Parsersettings.on_headers_complete = [](http_parser* parser) -> int
+    {
+        return Parse_Headerscomplete(parser, (HTTPRequest *)parser->data);
+    };
+    Parsersettings.on_body = [](http_parser* parser, const char* at, size_t len) -> int
+    {
+        return Parse_Body(parser, (HTTPRequest *)parser->data, at, len);
+    };
+    Parsersettings.on_message_complete = [](http_parser* parser) -> int
+    {
+        return Parse_Messagecomplete(parser, (HTTPRequest *)parser->data);
+    };
+};
+IHTTPSServer::IHTTPSServer() : ITLSServer() 
+{
+    http_parser_init(&Parser, HTTP_BOTH);
+    http_parser_settings_init(&Parsersettings);
+    Parsedrequest.Parsed = false;
+    Parser.data = &Parsedrequest;
+
+    Parsersettings.on_message_begin = [](http_parser *parser) -> int
+    {
+        return Parse_Messagebegin(parser, (HTTPRequest *)parser->data);
+    };
+    Parsersettings.on_url = [](http_parser* parser, const char* at, size_t len) -> int
+    {
+        return Parse_URL(parser, (HTTPRequest *)parser->data, at, len);
+    };
+    Parsersettings.on_header_field = [](http_parser* parser, const char* at, size_t len) -> int
+    {
+        return Parse_Headerfield(parser, (HTTPRequest *)parser->data, at, len);
+    };
+    Parsersettings.on_header_value = [](http_parser* parser, const char* at, size_t len) -> int
+    {
+        return Parse_Headervalue(parser, (HTTPRequest *)parser->data, at, len);
+    };
+    Parsersettings.on_headers_complete = [](http_parser* parser) -> int
+    {
+        return Parse_Headerscomplete(parser, (HTTPRequest *)parser->data);
+    };
+    Parsersettings.on_body = [](http_parser* parser, const char* at, size_t len) -> int
+    {
+        return Parse_Body(parser, (HTTPRequest *)parser->data, at, len);
+    };
+    Parsersettings.on_message_complete = [](http_parser* parser) -> int
+    {
+        return Parse_Messagecomplete(parser, (HTTPRequest *)parser->data);
+    };
+};
+IHTTPSServer::IHTTPSServer(const char *Hostname) : ITLSServer(Hostname) 
+{
+    http_parser_init(&Parser, HTTP_BOTH);
+    http_parser_settings_init(&Parsersettings);
+    Parsedrequest.Parsed = false;
+    Parser.data = &Parsedrequest;
+
+    Parsersettings.on_message_begin = [](http_parser *parser) -> int
+    {
+        return Parse_Messagebegin(parser, (HTTPRequest *)parser->data);
+    };
+    Parsersettings.on_url = [](http_parser* parser, const char* at, size_t len) -> int
+    {
+        return Parse_URL(parser, (HTTPRequest *)parser->data, at, len);
+    };
+    Parsersettings.on_header_field = [](http_parser* parser, const char* at, size_t len) -> int
+    {
+        return Parse_Headerfield(parser, (HTTPRequest *)parser->data, at, len);
+    };
+    Parsersettings.on_header_value = [](http_parser* parser, const char* at, size_t len) -> int
+    {
+        return Parse_Headervalue(parser, (HTTPRequest *)parser->data, at, len);
+    };
+    Parsersettings.on_headers_complete = [](http_parser* parser) -> int
+    {
+        return Parse_Headerscomplete(parser, (HTTPRequest *)parser->data);
+    };
+    Parsersettings.on_body = [](http_parser* parser, const char* at, size_t len) -> int
+    {
+        return Parse_Body(parser, (HTTPRequest *)parser->data, at, len);
+    };
+    Parsersettings.on_message_complete = [](http_parser* parser) -> int
+    {
+        return Parse_Messagecomplete(parser, (HTTPRequest *)parser->data);
+    };
+};
+IHTTPSServer::IHTTPSServer(const char *Hostname, const char *Certificate, const char *Key) : ITLSServer(Hostname) 
+{
+    http_parser_init(&Parser, HTTP_BOTH);
+    http_parser_settings_init(&Parsersettings);
+    Parsedrequest.Parsed = false;
+    Parser.data = &Parsedrequest;
+
+    Parsersettings.on_message_begin = [](http_parser *parser) -> int
+    {
+        return Parse_Messagebegin(parser, (HTTPRequest *)parser->data);
+    };
+    Parsersettings.on_url = [](http_parser* parser, const char* at, size_t len) -> int
+    {
+        return Parse_URL(parser, (HTTPRequest *)parser->data, at, len);
+    };
+    Parsersettings.on_header_field = [](http_parser* parser, const char* at, size_t len) -> int
+    {
+        return Parse_Headerfield(parser, (HTTPRequest *)parser->data, at, len);
+    };
+    Parsersettings.on_header_value = [](http_parser* parser, const char* at, size_t len) -> int
+    {
+        return Parse_Headervalue(parser, (HTTPRequest *)parser->data, at, len);
+    };
+    Parsersettings.on_headers_complete = [](http_parser* parser) -> int
+    {
+        return Parse_Headerscomplete(parser, (HTTPRequest *)parser->data);
+    };
+    Parsersettings.on_body = [](http_parser* parser, const char* at, size_t len) -> int
+    {
+        return Parse_Body(parser, (HTTPRequest *)parser->data, at, len);
+    };
+    Parsersettings.on_message_complete = [](http_parser* parser) -> int
+    {
+        return Parse_Messagecomplete(parser, (HTTPRequest *)parser->data);
+    };
+};
---
+++ Source/Servers/IHTTPServer.h
@@ -0,0 +1,66 @@
+/*
+    Initial author: (https://github.com/)Convery for Ayria.se
+    License: LGPL 3.0
+    Started: 2016-5-6
+    Notes:
+        Implements parsing of the HTTP protocol.
+*/
+
+#pragma once
+#include <Configuration\All.h>
+#include <Servers\ITCPServer.h>
+#include <Servers\ITLSServer.h>
+#include <HTTP\http_parser.h>
+
+typedef struct HTTPHeader
+{
+    std::string Field;
+    std::string Value;
+};
+typedef struct HTTPRequest
+{
+    bool Parsed;
+    std::string URL;
+    std::string Method;
+    std::vector<HTTPHeader> Headers;
+    std::string Body;
+};
+
+// Standard HTTP.
+struct IHTTPServer : public ITCPServer
+{
+    // Callbacks on data.
+    virtual void onGET(std::string &URL, std::string Body) = 0;
+    virtual void onPUT(std::string &URL, std::string Body) = 0;
+    virtual void onPOST(std::string &URL, std::string Body) = 0;
+    virtual void onStreamupdated(std::vector<uint8_t> &Incomingstream) override;
+
+    // Local parser.
+    http_parser Parser;
+    HTTPRequest Parsedrequest;
+    http_parser_settings Parsersettings;
+    
+    // Construct the server from a hostname.
+    IHTTPServer();
+    IHTTPServer(const char *Hostname);
+};
+
+// Encrypted HTTP.
+struct IHTTPSServer : public ITLSServer
+{
+    // Callbacks on data.
+    virtual void onGET(std::string &URL, std::string Body) = 0;
+    virtual void onPUT(std::string &URL, std::string Body) = 0;
+    virtual void onPOST(std::string &URL, std::string Body) = 0;
+    virtual void onStreamdecrypted(std::string &Incomingstream) override;
+
+    // Local parser.
+    http_parser Parser;
+    HTTPRequest Parsedrequest;
+    http_parser_settings Parsersettings;
+
+    // Construct the server from a hostname.
+    IHTTPSServer();
+    IHTTPSServer(const char *Hostname);
+    IHTTPSServer(const char *Hostname, const char *Certificate, const char *Key);
+};
