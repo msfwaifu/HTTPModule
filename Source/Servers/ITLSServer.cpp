e2002d937ca271b1deb577e65b576b4d0c93e6a8
Convery <convery10122@gmail.com>
2016-05-06 19:06:27 +0200

    TLS now behaves properly.

---
+++ Source/Servers/ITLSServer.cpp
@@ -0,0 +1,112 @@
+/*
+    Initial author: (https://github.com/)Convery for Ayria.se
+    License: LGPL 3.0
+    Started: 2016-5-6
+    Notes:
+        Buffered server with a callback on incoming data.
+*/
+
+#include <Configuration\All.h>
+#include <Servers\ITLSServer.h>
+
+// Callback and methods to insert data.
+void ITLSServer::Senddata(std::string &Databuffer) {};
+void ITLSServer::Senddata(const void *Databuffer, const size_t Datalength) {};
+void ITLSServer::onStreamupdated(std::vector<uint8_t> &Incomingstream) 
+{
+    size_t Readcount;
+
+    // Debugging vars.
+    BIO *Write_BIO = GetServerinfo()->Read_BIO;
+    BIO *Read_BIO = GetServerinfo()->Write_BIO;  
+
+    // Insert the data into the SSL buffer.
+    BIO_write(GetServerinfo()->Read_BIO, Incomingstream.data(), Incomingstream.size());
+    Incomingstream.clear();
+
+    if (!SSL_is_init_finished(GetServerinfo()->State))
+    {
+        size_t Resultcode = SSL_do_handshake(GetServerinfo()->State);
+        if (Resultcode != 1) DebugPrint(va("OpenSSL error: %s", ERR_error_string(Resultcode, NULL)));
+    }
+    else
+    {
+        auto Buffer = std::make_unique<char[]>(1024);
+        Readcount = SSL_read(GetServerinfo()->State, Buffer.get(), 1024);
+
+        if (Readcount > 0)
+        {
+            std::string Request(Buffer.get(), Readcount);
+            onStreamdecrypted(Request);
+        }
+    }
+
+    Syncbuffers();
+};
+void ITLSServer::Syncbuffers()
+{
+    if (!Streamguard.try_lock())
+        Streamguard.unlock();
+
+    Streamguard.lock();
+    {
+        auto Buffer = std::make_unique<char[]>(4096);
+        int Readcount = BIO_read(GetServerinfo()->Write_BIO, Buffer.get(), 4096);
+        if(Readcount > 0)
+            Outgoingstream.insert(Outgoingstream.end(), Buffer.get(), Buffer.get() + Readcount);
+    }
+    
+    // Unlocked when the function returns.
+}
+
+// Construct the server from a hostname.
+ITLSServer::ITLSServer() : ITCPServer() {};
+ITLSServer::ITLSServer(const char *Hostname) : ITCPServer(Hostname) {};
+ITLSServer::ITLSServer(const char *Hostname, const char *Certificate, const char *Key) : ITCPServer(Hostname) 
+{
+    size_t Resultcode;
+
+    // Initialize OpenSSL.
+    {
+        SSL_library_init();
+        SSL_load_error_strings();
+        ERR_load_BIO_strings();
+        OpenSSL_add_all_algorithms();
+    }
+
+    // Initialize the context.
+    {
+        GetServerinfo()->Context = SSL_CTX_new(TLSv1_server_method());
+        SSL_CTX_set_verify(GetServerinfo()->Context, SSL_VERIFY_NONE, NULL);
+    }
+
+    // Load the certificate and key for this server.
+    {
+        Resultcode = SSL_CTX_use_certificate_file(GetServerinfo()->Context, Certificate, SSL_FILETYPE_PEM);
+        if (Resultcode != 1) DebugPrint(va("OpenSSL error: %s", ERR_error_string(Resultcode, NULL)));
+
+        Resultcode = SSL_CTX_use_PrivateKey_file(GetServerinfo()->Context, Key, SSL_FILETYPE_PEM);
+        if (Resultcode != 1) DebugPrint(va("OpenSSL error: %s", ERR_error_string(Resultcode, NULL)));
+
+        Resultcode = SSL_CTX_check_private_key(GetServerinfo()->Context);
+        if (Resultcode != 1) DebugPrint(va("OpenSSL error: %s", ERR_error_string(Resultcode, NULL)));
+    }
+
+    // Create the BIO buffers.
+    {
+        GetServerinfo()->Write_BIO = BIO_new(BIO_s_mem());
+        GetServerinfo()->Read_BIO = BIO_new(BIO_s_mem());
+        BIO_set_nbio(GetServerinfo()->Write_BIO, 1);
+        BIO_set_nbio(GetServerinfo()->Read_BIO, 1);
+    }
+
+    // Initialize the SSL state.
+    {
+        GetServerinfo()->State = SSL_new(GetServerinfo()->Context);
+        if(!GetServerinfo()->State) DebugPrint("OpenSSL error: Failed to create the SSL state.");
+
+        SSL_set_bio(GetServerinfo()->State, GetServerinfo()->Read_BIO, GetServerinfo()->Write_BIO);        
+        SSL_set_verify(GetServerinfo()->State, SSL_VERIFY_NONE, NULL);
+        SSL_set_accept_state(GetServerinfo()->State);
+    }
+}
--- Source/Servers/ITLSServer.h
+++ Source/Servers/ITLSServer.h
@@ -11,28 +11,27 @@
 #include <Servers\ITCPServer.h>
 #include <openssl\bio.h>
 #include <openssl\ssl.h>
+#include <openssl\err.h>
 
 // Server-information that can be extended in other classes.
 #pragma pack(push, 1)
 struct ITLSServerinfo : public ITCPServerinfo
 {
     SSL_CTX *Context;
-    BIO *SSL_BIO;
-    BIO *APP_BIO;
+    BIO *Write_BIO;
+    BIO *Read_BIO;    
     SSL *State;
 };
 #pragma pack(pop)
 
 struct ITLSServer : public ITCPServer
 {
-    // Single-socket operations.
-    virtual void onConnect(const size_t Socket, const uint16_t Port) override;
-
     // Callback and methods to insert data.
     virtual void Senddata(std::string &Databuffer) override;
     virtual void Senddata(const void *Databuffer, const size_t Datalength) override;
     virtual void onStreamupdated(std::vector<uint8_t> &Incomingstream) override;
     virtual void onStreamdecrypted(std::string &Incomingstream) = 0;
+    virtual void Syncbuffers();
 
     // Server information in a raw format.
     ITLSServerinfo *GetServerinfo()
